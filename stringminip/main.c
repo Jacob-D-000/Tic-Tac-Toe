f8ef21056702346bd30a6aae5ea56b3f58020af6
w0469100 <114405256+w0469100@users.noreply.github.com>
2023-02-03 15:31:31 -0400

    Add files via upload

---
+++ stringminip/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char strA[] = "Hello\n";
+    char strB[] = "liam\n";
+    char strC[14];
+
+    strcat()
+    printf("%s\n", strA);
+    printf(strA);
+    printf(strB);
+    printf(strC);
+}
