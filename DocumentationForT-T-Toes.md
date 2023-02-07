6521fc04af05c499e4f9ba5baf9c69c33ad1e4b6
Jacob-D-000 <w0478482@nscc.ca>
2023-02-07 17:04:50 -0400

    This is getting out hand

--- DocumentationForT-T-Toes.md
+++ DocumentationForT-T-Toes.md
@@ -2,3 +2,7 @@
 This file will contain all documentation pertaining to my code for the tic tac toe project in C
 
 I will do my best to keep a log of the times I work on this project. Look at the commit list to see how the work has developed.
+
+4:00 - 5:000
+
+running into some issues with too many arrays. Going to work on it some more when I get hone
--- Tic-Tac-Toe/main.c
+++ Tic-Tac-Toe/main.c
@@ -3,26 +3,52 @@
 
 int main()
 {
-    // top and botten of the grid
-    char tline [] = {"+===1===|===2===|===3===+\n"};
-    char eline [] = ("+=======|=======|=======+");
+    // top and botten of the grid array
+    char tline [] = {"   ___1_____2_____3___\n"};
+    char eline [] = {"   |=====|=====|=====|\n"};
     // blank left side
-    char aside [] = {"A       "};
-    char bside [] = {"B       "};
-    char cside [] = {"C       "};
-    char mblank [] = {"       "};
-    char gird[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    char aside [] =
+    char bside [] =
+    char cside [] =
+    char mblank [] =
+    // Grid array for users to input
+    char grid[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    // art for x and o's
+    char O[3][5] = {"", "", ""};
+    char X[3][5] = {{'', '', '', '', ''}, {'', '', '', '', ''}, {'', '', '', '', ''}};
+    // array to hold y - axis values
+    char yaxis[3] = {'A', 'B', 'C'};
     //control varibles
     int i;
     int j;
     // k will be used spefcifkly for the user input grid
-    int k = 0
+    int k = 0;
+    // l will be used to thorugh an array of letters
+    int l = 0;
+    // print top of grid before you loop
     printf(tline);
+    printf(t2andeline);
+    // start for loop the print each line
     for (i=0; i <11; i++)
-        for (j = 0; j < 2; j++)
-            if
+    {
+        if (i == 3 || i == 7)
+            printf(eline);
+        else
+        {
+            if (i == 1 || i == 5 || i == 9 );
+                printf(" %c|", yxais[l]);
+                l++;
+            if ()
+            for (j = 0; j < 3; j++)
+                if (grid[k][j] == x)
 
 
+                else if ( (grid[k][j] == y)
 
-    if (i == )
+                else
+        }
+
+        if (i == )
+    }
+    printf(t2andeline);
 }
