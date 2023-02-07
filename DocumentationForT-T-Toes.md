9f34d2bda3ab51773fda98fbc5db7c9ef9d7f7d8
Jacob-D-000 <w0478482@nscc.ca>
2023-02-07 15:12:29 -0400

    First commit of work. Will do some more work later today

---
+++ DocumentationForT-T-Toes.md
@@ -0,0 +1,4 @@
+# Toe Documentation.
+This file will contain all documentation pertaining to my code for the tic tac toe project in C
+
+I will do my best to keep a log of the times I work on this project. Look at the commit list to see how the work has developed.
---
+++ Tic-Tac-Toe/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // top and botten of the grid
+    char tline [] = {"+===1===|===2===|===3===+\n"};
+    char eline [] = ("+=======|=======|=======+");
+    // blank left side
+    char aside [] = {"A       "};
+    char bside [] = {"B       "};
+    char cside [] = {"C       "};
+    char mblank [] = {"       "};
+    char gird[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    //control varibles
+    int i;
+    int j;
+    // k will be used spefcifkly for the user input grid
+    int k = 0
+    printf(tline);
+    for (i=0; i <11; i++)
+        for (j = 0; j < 2; j++)
+            if
+
+
+
+    if (i == )
+}
